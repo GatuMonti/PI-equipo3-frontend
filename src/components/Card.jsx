dbe49afe5e32a803e046b15f082d92a97753b9aa
JGalvisS <mejorquemada@hotmail.com>
2024-03-15 11:42:54 -0500

    ajustar cards home

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,21 @@ import { Link } from 'react-router-dom'
 
 function Card({product}) {
 
-    console.log(product.images)
-    
+  console.log(product.images)
+  // Limita la cantidad de caracteres de DescriptionProduct 
+  const maxLength = 75;
+
+  const trimmedDescription =
+    product.description.length > maxLength
+      ? product.description.substring(0, maxLength) + '...'
+      : product.description;
 
   return (
     <div className='card'>
         <Link to={'/Detail/' + product.id}>
            <img className="imageProduct" src={product.images[0].imageUrl}alt="imagen del producto" />
            <h5 className='tituloProducto'> {product.name} </h5>
-           <h3 className='tituloProducto'><b>{product.price} USD </b></h3>
+           <p className="DescriptionProduct">{trimmedDescription}</p>
         </Link>
     </div>
   )
